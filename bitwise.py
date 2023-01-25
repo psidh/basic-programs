3fbf3784473db1bde428a22881d7e2155faa920d
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-01-25 09:25:52 +0530

    Update bitwise.py

--- bitwise.py
+++ bitwise.py
@@ -24,5 +24,16 @@ f = int(input())
 print(e>>1)
 print(f<<3)
 
-#PROGRAM 4: 
+#PROGRAM 4: REVISION
 
+
+cond = input("Enter from what you want to convert mtokm or kmtomi : ")
+
+if (cond=="mtokm"):
+  m = int(input("Enter the number of miles: "))
+  print(f"{m} miles is equal to {m * 1.6}")
+elif (cond=="kmtom"):
+  m = int(input("Enter the number of kilometres: "))
+  print(f"{km} miles is equal to {km / 1.6}")
+else: 
+  print("Sorry you have not given the apt input to the system.")
