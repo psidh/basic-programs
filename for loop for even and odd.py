a9f23360ef29793aeadbc3f56cc10031431c87a8
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-01-30 09:24:34 +0530

    Add files via upload

---
+++ for loop for even and odd.py
@@ -0,0 +1,20 @@
+
+print("Thebelow are the even numbers: ")
+
+for i in range(0,n):
+    if (i%2) == 0;
+      print(i)
+    else:
+      continue
+
+
+i=1
+
+print("The below are the odd numbers: \n")
+
+for i in range(0,n):
+    if (i%2) != 0;
+      print(i)
+    else:
+      continue
+
