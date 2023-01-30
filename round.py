8ba1586358eea5e42cfe91e42f82c51b2e07fbd3
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-01-30 09:04:45 +0530

    Add files via upload

---
+++ round.py
@@ -0,0 +1,23 @@
+print('Learning')
+
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+c = int(input("Enter a number: "))
+
+
+print(eval(input(a+b+c)))
+
+# #round function evaluates, the number of decimal places around the expression taken as an input
+
+
+
+i  = 1
+for i in range(10):
+    print(i)
+
+print("___________________")
+
+i=1
+while (i<=10):
+    print(i)
+    i+= 1
