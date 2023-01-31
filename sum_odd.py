7707dd56df682fccf6b92cc3bf1314c11b4681bb
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-01-31 11:22:44 +0530

    Create sum_odd.py

---
+++ sum_odd.py
@@ -0,0 +1,10 @@
+start = int(input("Enter a start range: "))
+end = int(input("Enter a end range: "))
+
+count = 0
+if start %2 != 0:
+    for j in range(start, end, 2):
+        count += j
+    print(f"{count} is the summ of odd numbers.")
+else:
+    pass
