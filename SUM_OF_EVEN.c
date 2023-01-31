83868a9dbe5a2456d7927857833ffeadd3e03bc9
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-01-31 11:21:00 +0530

    Add files via upload

---
+++ SUM_OF_EVEN.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int i, start, end, sum=0;
+
+    /* Input lower and upper limit from user */
+    printf("Enter lower limit: ");
+    scanf("%d", &start);
+    printf("Enter upper limit: ");
+    scanf("%d", &end);
+
+
+    /* If start is not even then make it even */
+    if(start%2!=0)
+    {
+        start++;
+    }
+
+    for(i=start; i<=end; i+=2)
+    {
+        /* Add current even number to sum */
+        sum += i;
+    }
+
+    printf("Sum of all even number between %d to %d = %d", start, end, sum);
+
+    return 0;
+}
