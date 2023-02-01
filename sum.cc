ef9a20a323b3362dbc46f6d53236f7a91b3cae10
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-02-01 08:29:38 +0530

    Add files via upload

---
+++ sum.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int first_number, second_number, sum;
+    
+  cout << "Enter first number: ";
+  cin >> first_number 
+
+  cout << "Enter first number: ";
+  cin >> second_number 
+
+  // sum of two numbers in stored in variable sumOfTwoNumbers
+  sum = first_number + second_number;
+
+  // prints sum 
+  cout << first_number << " + " <<  second_number << " = " << sum;     
+
+  return 0;
+}
