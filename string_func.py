a0caa75e7aa449c33b275884a09d64cc0ccc280e
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-02-01 08:18:43 +0530

    Add files via upload

---
+++ string_func.py
@@ -0,0 +1,11 @@
+#strings and their functions 1/2/2023
+
+str = input('Enter a string: ')
+print(str.capitalize())
+print(str.casefold())
+print(str.count("a"))
+print(str.capitalize())
+
+if str.endswith("s"):
+    print("True")
+
