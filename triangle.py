75fb1bb93d7207ec254cd925c8891b0d5866b666
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-02-04 17:12:21 +0530

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,8 @@
+print("This program calculates the third angle of a triangle.")
+
+f= int(input("Enter the first angle: "))
+
+s= int(input("Enter the second angle: "))
+
+t= 180 - (f +s )
+print(f"The third angle is {t} ")
