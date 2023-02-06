677ce499a7f8c0d1520df7dbb69fb3a927e52f3f
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-02-06 08:56:38 +0530

    Update string_func.py

--- string_func.py
+++ string_func.py
@@ -27,3 +27,20 @@ print(f"the strings wiht he followong values in it: {str.find}")
 string1 = input("Enter a string to operate some of the functions: ")
 
 print(f" Check for numberical spaces in a string {string1.isspace}") ## boolean operator on a string
+
+pan_number = input("Enter a string: ")
+
+# def is_Valid_pan_alpha(pan_number):
+#     pan_number.isalpha and pan_number.isupper
+  
+
+# def check_pan():
+#     if pan_number.length == 10 and is_Valid_pan_alpha(pan_number) and pan_number[5:8].isnum == "True":
+       
+
+def check_pan():
+    if pan_number.isupper =="True" and pan_number.length == 10 and pan_number[0:4].isalpha == "True" and pan_number[5:8].isnum == "True" and pan_number[10].isalpha == "True":
+        print("Pan Card number is a valid one ")
+    else:
+        print("Invalid PAN card number")
+       
