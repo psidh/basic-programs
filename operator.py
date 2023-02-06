cb2f4091f135b626d045586c6c103a19754eeff5
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-02-06 08:13:30 +0530

    Create operator.py

---
+++ operator.py
@@ -0,0 +1,7 @@
+# operator in python class date - 6th Feb 2023
+
+# membership and identity operators exist in python
+
+#membership used in for and while loops as an iterator and called or termed as a member in the range or list
+
+
