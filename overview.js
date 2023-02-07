961f13650267672a332a312405b34a365403e588
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-02-07 09:09:29 +0530

    Update overview.js

--- overview.js
+++ overview.js
@@ -496,4 +496,13 @@ catch(err) {
 </script>
 //____________________________________________________________________
 
-/*
+//Scope in JAVASCRIPT
+// code here can NOT use carName
+
+function myFunction() {
+  let carName = "Volvo";
+  // code here CAN use carName
+}
+
+// code here can NOT use carName
+
