7a1e992bce9876282102671b69242511c3ddbb8c
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-02-07 10:13:27 +0530

    Update math_func.py

--- math_func.py
+++ math_func.py
@@ -11,3 +11,7 @@ print(f"The cos value pof the given input is {cos(number)}")
 print(f"The cos value pof the given input is {tan(number)}")
 
 print(f"Exponential of {number} is {mt.exp(number)}")
+
+print(f"natural logarithm of {number} is {mt.log(number)}")
+
+print(f"logarithm of {number} is {mt.log10(number)}")
