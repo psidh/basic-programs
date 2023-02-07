22bd1b28fa8692abba9c084956e8313214cf2309
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-02-07 10:12:21 +0530

    Update math_func.py

--- math_func.py
+++ math_func.py
@@ -9,3 +9,5 @@ print(f"The sin value of the given input is {sin(number)}")
 print(f"The cos value pof the given input is {cos(number)}")
 
 print(f"The cos value pof the given input is {tan(number)}")
+
+print(f"Exponential of {number} is {mt.exp(number)}")
