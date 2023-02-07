d8eda8dc619353ee11c4d922e563090553a4384d
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-02-07 10:10:16 +0530

    Create math_func.py

---
+++ math_func.py
@@ -0,0 +1,11 @@
+from math import *
+
+constant_pi = pi
+
+number = int(input("Enter a number:  "))
+
+print(f"The sin value of the given input is {sin(number)}")
+
+print(f"The cos value pof the given input is {cos(number)}")
+
+print(f"The cos value pof the given input is {tan(number)}")
