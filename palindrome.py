28a711b7029fd8e09b11aba7e1f70db829507a0c
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-02-08 09:04:39 +0530

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,13 @@
+def isPalindrome(s):
+    s1str = str(s)
+    return s1str == s1str[::-1]
+ 
+ 
+
+s = int(input("Enter a string: "))
+ans = isPalindrome(s)
+ 
+if ans:
+    print("Yes")
+else:
+    print("No")
