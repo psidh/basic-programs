0e18cde43494dc7f38a8c49e24a62365c2cc8d7d
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-02-08 08:32:53 +0530

    Update fib.py

--- fib.py
+++ fib.py
@@ -23,3 +23,21 @@ else:
        n1 = n2
        n2 = nth
        count += 1
+
+# next method using the concept ofn function
+
+
+def recur_fibo(n):  
+   if n <= 1:  
+       return n  
+   else:  
+       return(recur_fibo(n-1) + recur_fibo(n-2))  
+# take input from the user  
+nterms = int(input("How many terms? "))  
+# check if the number of terms is valid  
+if nterms <= 0:  
+   print("Plese enter a positive integer")  
+else:  
+   print("Fibonacci sequence:")  
+   for i in range(nterms):  
+       print(recur_fibo(i))  
