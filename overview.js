01b396f5784227e9468b9c17d7f71be9727c635b
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-02-08 08:11:34 +0530

    Update overview.js

--- overview.js
+++ overview.js
@@ -603,3 +603,10 @@ document.getElementById("demo").innerHTML =
 import message from "./message.js";
 </script> */
 
+const message = () => {
+  const name = "Jesse";
+  const age = 40;
+  return name + ' is ' + age + 'years old.';
+  };
+  
+  export default message;
