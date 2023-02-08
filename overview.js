dab2d3593bf9c92c6bf733d2e0c103798dde74af
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-02-08 08:17:09 +0530

    Update overview.js

--- overview.js
+++ overview.js
@@ -750,3 +750,16 @@ A Server API can extend the functionality of a web server. */
 
 //const myElement = document.getElementById("demo");
 
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    myElement.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  myElement.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude; 
+} 
+
