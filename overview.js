c5bbca8b275261860928a6f8eafb56c86ac00871
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-02-08 08:17:22 +0530

    Update overview.js

--- overview.js
+++ overview.js
@@ -763,3 +763,39 @@ function showPosition(position) {
   "<br>Longitude: " + position.coords.longitude; 
 } 
 
+
+// Geolocation API
+// cannnot be accessed by all, cause of privacy issues
+
+// /* <!DOCTYPE html>
+// <html>
+// <body>
+
+// <h2>JavaScript Geolocation API</h2>
+// <p>Click the button to get your coordinates.</p>
+
+// <button onclick="getLocation()">Try It</button>
+
+// <p id="demo"></p>
+
+// <script>
+// const x = document.getElementById("demo");
+
+// function getLocation() {
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(showPosition);
+//   } else { 
+//     x.innerHTML = "Geolocation is not supported by this browser.";
+//   }
+// }
+
+// function showPosition(position) {
+//   x.innerHTML = "Latitude: " + position.coords.latitude + 
+//   "<br>Longitude: " + position.coords.longitude;
+// }
+// </script>
+
+// </body>
+// </html>
+//  */
+
