6063d3e46907ef7f73e2c370e3581bf4c5ca6228
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-02-08 08:14:29 +0530

    Update overview.js

--- overview.js
+++ overview.js
@@ -637,3 +637,49 @@ obj.employees[1].firstName + " " + obj.employees[1].lastName;
 </script> */
 
 //__________________________________________
+
+// Create object with an existing object as prototype
+Object.create()
+
+// Adding or changing an object property
+Object.defineProperty(object, property, descriptor)
+
+// Adding or changing object properties
+Object.defineProperties(object, descriptors)
+
+// Accessing Properties
+Object.getOwnPropertyDescriptor(object, property)
+
+// Returns all properties as an array
+Object.getOwnPropertyNames(object)
+
+// Accessing the prototype
+Object.getPrototypeOf(object)
+
+// Returns enumerable properties as an array
+Object.keys(object)
+
+//___________________________________________
+
+// protecting objects from any interference
+
+// Prevents adding properties to an object
+Object.preventExtensions(object)
+
+// Returns true if properties can be added to an object
+Object.isExtensible(object)
+
+// Prevents changes of object properties (not values)
+Object.seal(object)
+
+// Returns true if object is sealed
+Object.isSealed(object)
+
+// Prevents any changes to an object
+Object.freeze(object)
+
+// Returns true if object is frozen
+Object.isFrozen(object)
+
+// The super() method refers to the parent class.
+// js callback are used for refering to the old definde functions
