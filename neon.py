c9f282ecdaa8836e2a11f1ff7dc25deb3b5fd44e
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-02-08 09:33:38 +0530

    Update neon.py

--- neon.py
+++ neon.py
@@ -11,3 +11,28 @@ if (num == sum):
     print(f"{num} is a Neon Number \n")
 else:
     print(f"{num} is not a Neon Number \n")
+
+#new method for calculating the neon number:
+
+
+num3 = int(input("Enter a number \n"))
+sqr2 = num**2 
+
+str = str(sqr2)
+
+inte = []
+
+for i in range(len(str)):
+    inte.append(int(str[i]))
+
+print(inte)
+num2 = 0
+for i in range(len(str)):
+    num2 += inte[i]
+
+print(num2)
+
+if (num3 == num2):
+    print("Neon Number \n")
+else:
+    print("Not a Neon Number \n")
