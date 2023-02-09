3dc4d067f4b368368afbc7688770c8eaa618b72f
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-02-09 10:08:58 +0530

    Update root_quadratic.py

--- root_quadratic.py
+++ root_quadratic.py
@@ -3,11 +3,13 @@ import cmath
 a = float(input('Enter a: '))  
 b = float(input('Enter b: '))  
 c = float(input('Enter c: '))  
+def root(a, b, c):
   
-# calculate the discriminant  
-d = (b**2) - (4*a*c)  
-  
-# find two solutions  
-sol1 = (-b-cmath.sqrt(d))/(2*a)  
-sol2 = (-b+cmath.sqrt(d))/(2*a)  
-print('The solution are {0} and {1}'.format(sol1,sol2))   
+  # calculate the discriminant  
+  d = (b**2) - (4*a*c)  
+
+  # find two solutions  
+  sol1 = (-b-cmath.sqrt(d))/(2*a)  
+  sol2 = (-b+cmath.sqrt(d))/(2*a)  
+  print('The solution are {0} and {1}'.format(sol1,sol2))   
+root(a, b, c)
