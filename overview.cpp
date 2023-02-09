3ee4f8b7cd992390c105df46322b8502c3f4aa90
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-02-09 10:28:22 +0530

    Update overview.cpp

--- overview.cpp
+++ overview.cpp
@@ -1,5 +1,12 @@
-
+#include <iostream>
 int main()
 
-{
+{   // define an integer variable named x
+    int x1 = 0; // this variable is uninitialized because we haven't given it a value
+    
+    // print the value of x to the screen
+    std::cout << x1 << '\n'; // who knows what we'll get, because x is uninitialized
+    
+    // there are 92 reserved words in C++
+    
     
