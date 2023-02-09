0c5d2b3490261b9f57aa5d5a1c00397cfa4f7423
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-02-09 13:19:43 +0530

    Create perfect.java

---
+++ perfect.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PerfectNumber {
+    public static boolean isPerfectNumber(int number) {
+        int sum = 1;
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                sum += i + number / i;
+            }
+        }
+        return sum == number;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        if (isPerfectNumber(number)) {
+            System.out.println(number + " is a perfect number.");
+        } else {
+            System.out.println(number + " is not a perfect number.");
+        }
+    }
+}
