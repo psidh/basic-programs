e0a0752c2f2b3475d58bf1ce14c838724266c3e1
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-02-09 10:05:07 +0530

    Update leapyear.py

--- leapyear.py
+++ leapyear.py
@@ -1,13 +1,16 @@
-if (year % 100==0 and year%400==0):
-  print("The year is leap year.")
+def check(year):
+  if (year % 100==0 and year%400==0):
+    print("The year is leap year.")
 
-elif(year%4==0):
-  print("THe year is leap year.")
+  elif(year%4==0):
+    print("THe year is leap year.")
 
-else:
-  print("not a leap year")
+  else:
+    print("not a leap year")
 
-
+year = int(input("Enter the year: "))
+check(year)
+ 
 # second method
 
 
