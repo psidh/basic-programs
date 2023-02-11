a61582234c5123d0fb0b7aa2aec658ce22f66ab4
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-02-11 10:58:51 +0530

    Update armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -4,14 +4,34 @@ num = int(input("Enter a number: "))
 sum = 0
 
 
-temp = num
+tempo = num
 while temp > 0:
-   digit = temp % 10
+   digit = tempo % 10
    sum += digit ** 3
-   temp //= 10
+   tempo //= 10
 
 
 if num == sum:
    print(num,"is an Armstrong number")
 else:
    print(num,"is not an Armstrong number")
+
+   
+   # n armstrong numbers between a given range
+number = int(input())
+
+string = str(number)
+length = len(string)
+
+for i in range(1, number):
+
+    temp = i
+    s=0
+    while(i>0):
+        rem = i%10
+        s = s+ (rem**length)
+        i //= 10
+    if (temp == s):
+        print(temp)
+    
+
