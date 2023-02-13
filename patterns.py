c36728198f2e61d99aa649206c9be0087dc72b28
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-02-13 09:18:50 +0530

    Update patterns.py

--- patterns.py
+++ patterns.py
@@ -35,3 +35,10 @@ for i in range(n+1):            #The first loop for printing the no. of rows
         else:
             print(" ", end = "")#The SPACES is printed to this
     print()
+    
+n= 1 
+for i in range(0,4):
+   for j in range(0,i+1):
+     print(n,end=" ")
+     n+=1
+   print()
