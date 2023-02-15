53dcffa8f2a4068d7275281a464922aad55e6c83
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-02-15 08:51:21 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -11,3 +11,23 @@ if ans:
     print("Yes")
 else:
     print("No")
+    
+n = int(input())
+
+def reverse(n):
+    length = len(str(n))
+    list = []
+    for i in range(length):
+        list.append(n%10)
+        n = n//10
+    palin=''
+    for i in range(len(list)):
+        palin += str(list[i])
+    print(palin)  
+    if int(palin) == n:
+        print("A Palindrome number")
+    else:
+        print("Not a Palindrome")  
+
+reverse(n)
+    
