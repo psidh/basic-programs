8b5303351a89897fd7e06d6c4c47eb3baa688606
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-02-21 10:09:15 +0530

    Update trash.py

--- trash.py
+++ trash.py
@@ -1,27 +1,12 @@
-#starting
-#20th feb -- the 19th feb was too good :)
-num = int(input("Enter a number: "))
-def arms(num):
-  sum = 0
-  tempo = num
-  while tempo > 0:
-    digit = tempo % 10
-    sum += digit ** (len(str(num)))
-    tempo //= 10
-  if num == sum:
-    return num,"is an Armstrong number"
-  else:
-    return num,"is not an Armstrong number"  
-ans = arms(num)
-print(ans)
- 
-def fact(num):
-    fact = 0
-    if num == 1:
-      return 1
-    for i in range(1, num -1):
-        fact *= i
-    return fact
-ans2 = fact(num)
-print(ans2)
+#21st Feb class
 
+#recursive functions 
+
+def factoirial(n):
+  i = 0
+  fact = 1
+  while(i<n):
+    fact*=i
+    i+=1
+  return fact
+  
