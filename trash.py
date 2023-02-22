9f9b719755fe9147445e519cb69fadd2e26c9c85
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-02-22 09:18:43 +0530

    Update trash.py

--- trash.py
+++ trash.py
@@ -8,4 +8,4 @@ print(f"The marks of the subjects are; ")
 for i in range(5):
   print(marks[i])
 print(f"the average of the marks are {sum(marks)/5}")  
-
+print(f"The percentage of {name} is {(sum(marks)/5)*100}")
