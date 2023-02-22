1eff12eed3e6506c71a8e63cc8d93f071fcafaf0
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-02-22 09:13:01 +0530

    Update trash.py

--- trash.py
+++ trash.py
@@ -1,1 +1,11 @@
 num = int(input())
+name = input()
+marks = []
+for i in range(5):
+  marks.append(input("Enter the marks: "))
+
+print(f"The marks of the subjects are; ")  
+for i in range(5):
+  print(marks[i])
+  
+
