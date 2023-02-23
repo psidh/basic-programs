403013518cb5fea898e297b3383c3e20e78e5193
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-02-23 14:54:14 +0530

    Update trash.py

--- trash.py
+++ trash.py
@@ -1,11 +1,1 @@
-num = int(input())
-name = input()
-marks = []
-for i in range(5):
-  marks.append(input("Enter the marks: "))
 
-print(f"The marks of the subjects are; ")  
-for i in range(5):
-  print(marks[i])
-print(f"the average of the marks are {sum(marks)/5}")  
-print(f"The percentage of {name} is {(sum(marks)/5)*100}")
