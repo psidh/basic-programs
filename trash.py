5eb5b20ae690dd3cd3d9dc29512431ff4f84b3cf
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-02-27 09:20:45 +0530

    Update trash.py

--- trash.py
+++ trash.py
@@ -2,3 +2,23 @@ n =. int(input())
 for i in range(n):
   list = []
   list.append(i)
+(X, Y, Z ) = (1, 2, 3)
+
+print(X)
+print(Y)
+
+tuple1 = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+list1 = list(tuple1)
+
+print(list1)
+list1[3]  = "4"
+list1.append(67)
+list1.remove(3)
+list1.pop()
+for i in range(len(list1)):
+    list1.append(i)
+
+print(tuple1)
+ 
+print(tuple1.count(2))
+
