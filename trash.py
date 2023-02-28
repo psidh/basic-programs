3629f90bbbc301081061ae0c25c3942693a3590d
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-02-28 10:18:21 +0530

    Update trash.py

--- trash.py
+++ trash.py
@@ -1,19 +1,28 @@
-(X, Y, Z ) = (1, 2, 3)
+LIST_OF_ALL_STUDENTS = []
+student_A = []
+student_B = []
+student_C = []
+strength_of_class = int(input("Enter the strength of the class: "))
 
-print(X)
-print(Y)
+for i in range(strength_of_class):
+    fname  =  input("Enter your first name: ")
+    mname  =  input("Enter your first name: ")
+    lname  =  input("Enter your first name: ")
 
-tuple1 = (1, 2, 3, 4, 5, 6, 7, 8, 9)
-list1 = list(tuple1)
+    marks  =  input("Enter your first name: ")
 
-print(list1)
-list1[3]  = "4"
-list1.append(67)
-list1.remove(3)
-list1.pop()
-for i in range(len(list1)):
-    list1.append(i)
+    name  = fname+ " " + mname + " " + lname
 
-print(tuple1)
- 
-print(tuple1.count(2))
+    LIST_OF_ALL_STUDENTS.append(name)
+    
+    if marks > 80:
+        student_A.append(name)
+    elif 70 < marks <= 80:
+        student_B.append(name)
+    else:
+        student_C.append(name)
+
+print(f"The list of all the students are \n {LIST_OF_ALL_STUDENTS} \n ")
+print(f"The list of A grade students \n {student_A}")
+print(f"The list of B grade students \n {student_B}")
+print(f"The list of C grade students \n {student_C}")
