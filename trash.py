31d2a29fdf39d957ffba19b7674c0d7c84581f9c
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-02-28 10:50:37 +0530

    Update trash.py

--- trash.py
+++ trash.py
@@ -1,4 +1,5 @@
-LIST_OF_ALL_STUDENTS = []
+STUDENTS = []
+
 student_A = []
 student_B = []
 student_C = []
@@ -6,23 +7,26 @@ strength_of_class = int(input("Enter the strength of the class: "))
 
 for i in range(strength_of_class):
     fname  =  input("Enter your first name: ")
-    mname  =  input("Enter your first name: ")
-    lname  =  input("Enter your first name: ")
+    mname  =  input("Enter your middle name: ")
+    lname  =  input("Enter your last name: ")
 
-    marks  =  input("Enter your first name: ")
+    marks  =  int(input("Enter your marks: "))
 
     name  = fname+ " " + mname + " " + lname
 
-    LIST_OF_ALL_STUDENTS.append(name)
-    
-    if marks > 80:
+    sub_list = [name, marks]
+    STUDENTS.append(sub_list)   
+
+for i in range(strength_of_class):
+    if STUDENTS[i][1] > 80:
         student_A.append(name)
-    elif 70 < marks <= 80:
+    
+    elif 70 <= STUDENTS[i][1] <= 80:
         student_B.append(name)
-    else:
+    elif 70 < STUDENTS[i][1]:
         student_C.append(name)
 
-print(f"The list of all the students are \n {LIST_OF_ALL_STUDENTS} \n ")
+print(f"The list of all the students are \n {STUDENTS}")
 print(f"The list of A grade students \n {student_A}")
 print(f"The list of B grade students \n {student_B}")
 print(f"The list of C grade students \n {student_C}")
