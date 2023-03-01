9a8c7da5ff6ef03e8c6d3c118702c47516daefd2
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-03-01 08:50:58 +0530

    Update trash.py

--- trash.py
+++ trash.py
@@ -1,1 +1,20 @@
+# import random
 
+# list1 = [1,2, 3, 4, 5, 6]
+
+# target =  int(input("Enter the target value: "))
+
+# def fact(n):
+#     for i in range(1, n+1):
+#         fact = 1
+#         fact *= i
+#     return fact
+# count = 0
+# for j in range(fact(len(list1))):
+#     k = random.randint(0, len(list1)-1)
+#     if list1[j] + list1[k] == target:
+#         count+=1 
+#         print(count) 
+
+
+list1 = 
