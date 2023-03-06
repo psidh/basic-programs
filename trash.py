17e717950dbc372d3e90a5fac89c4fc3f008cada
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-03-06 09:22:54 +0530

    Update trash.py

--- trash.py
+++ trash.py
@@ -1,13 +1,17 @@
-
 DICT = {}
+def read():
+    for i in range(15):
+        n = int(input("Enter the key: "))
+        m = int(input("Enter the value: "))
 
-for i in range(15):
-    n = int(input("Enter the key: "))
-    m = int(input("Enter the value: "))
+        DICT[n] = m
 
-    DICT[n] = m
+    return (DICT)
 
-print(DICT)
+dictionary = read()
+print(dictionary)
 
 def rad_eval():
     return(eval(input("Enter the dictionary values: ")))
+
+    
