56ca7f789e46533f5e1fbced792fd8dc8ae70251
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-03-07 11:08:35 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,14 @@
+
+# ● Program to get the difference between the two lists.
+
+lk = [1, 2, 3, 4, 5]
+lo = [6, 5, 4, 3, 2]
+
+for i in range(5):
+    if lk[i] not in lo:
+        l3 = []
+        l3.append(lk[i])
+    else:
+        pass
+print(l3)
+
