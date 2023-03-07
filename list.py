a0582c0d549fb0b74f0c890b9b7fbbdd477fc50c
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-03-07 11:34:23 +0530

    Update list.py

--- list.py
+++ list.py
@@ -12,3 +12,26 @@ for i in range(5):
         pass
 print(l3)
 
+
+list2=[]
+def i():
+    rows = int(input("Enter thh number of rows: "))
+    
+    for i in range(rows):
+        input_ = input("Enter the row sequence")
+        k = input_.split()
+        list2.append(k)
+
+i()
+print(list2)
+
+list3 = []
+for row in l1:
+    boy = row.count(0)
+    girl = row.count(1)
+    list3.append([boy, girl])
+
+for i in range(len(list3)):
+    
+    print(f"{i+1} Row: boys {list3[i][0]}")
+    print(f"{i+1} Row: girls {list3[i][1]}")
