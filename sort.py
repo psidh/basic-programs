1d5223f4c3e9dea81c576f0cb79eb203526e9954
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-03-07 10:40:17 +0530

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,5 +1,59 @@
+# list1 = [1, 2, 3, 4, 5]
 
-#Q2) read two lists - sort individualy - -> merge the lists and display  
+# if len(list1) % 2 != 0:
+#     list2 = list1[:-1]  # create a copy of list1 without the last element
+#     for i in range(len(list2)):
+#         if i % 2 == 0:
+#             list2[i+1], list2[i] = list2[i], list2[i+1]
+    
+#     list2.append(list1[-1])  # add back the last element
+
+#     print(list2)
+# else:
+#     print("List length must be odd for this operation.")
+
+# #________________________________________________________________
+
+
+# string1 = input()
+# last_letter = string1[-1]
+# lst = list(string1)
+
+# for letter in string1:
+#    if string1.count(letter) > 1:
+#         first_index = string1.find(letter) # Find the first occurrence
+#         second_index = string1.find(letter, first_index+1) # Find the second occurrence starting from the index of the first occurrence
+
+#         lst[second_index] = "$"
+#         lst[-1] = last_letter
+
+# modified = ''.join(lst)
+
+# print(modified)
+
+#__________________________________________________________________
+
+# lst = [-2, -3, 1, -5, 2, -3]
+# list2 = []
+# for i in range(len(lst)):
+#     if lst[i] > 0 and lst[i+1] > 0:
+        
+#         list2.append(lst[i])
+#         list2.append(lst[i+1])
+#     else:
+#         pass
+        
+# print(list2)
+
+
+
+#__________________________________________________________________
+
+
+#CONSTANT = 3816547290
+
+
+#Q1) read two lists - sort individualy - -> merge the lists and display  
 #_______________________________________________________
 
 l1 = [5, 4, 3]
